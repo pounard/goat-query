ab7a7c9851696699063b03668d851c577485e0ed
Pierre Rineau <pierre.rineau@makina-corpus.com>
2020-03-11 10:00:38 +0100

    few cleanups after rebase

--- src/Runner/Testing/NullRunner.php
+++ src/Runner/Testing/NullRunner.php
@@ -4,29 +4,11 @@ declare(strict_types=1);
 
 namespace Goat\Runner\Testing;
 
-use Goat\Driver\Platform\Escaper\Escaper;
 use Goat\Driver\Runner\AbstractRunner;
 use Goat\Runner\ResultIterator;
-use Goat\Runner\Transaction;
 
 class NullRunner extends AbstractRunner
 {
-    /**
-     * {@inheritdoc}
-     */
-    protected function getEscaper(): Escaper
-    {
-        return $this->escaper ?? ($this->escaper = new NullEscaper());
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function supportsDeferingConstraints(): bool
-    {
-        return true;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -38,71 +20,7 @@ class NullRunner extends AbstractRunner
     /**
      * {@inheritdoc}
      */
-    public function escapeLiteral(string $string): string
-    {
-        return $this->getEscaper()->escapeLiteral($string);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function writePlaceholder(int $index): string
-    {
-        return $this->getEscaper()->writePlaceholder($index);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function unescapeBlob($resource): ?string
-    {
-        return $this->getEscaper()->unescapeBlob($resource);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function escapeIdentifier(string $string): string
-    {
-        return $this->getEscaper()->escapeIdentifier($string);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function escapeLike(string $string): string
-    {
-        return $this->getEscaper()->escapeLike($string);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function escapeBlob(string $word): string
-    {
-        return $this->getEscaper()->escapeBlob($word);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getEscapeSequences(): array
-    {
-        return $this->getEscaper()->getEscapeSequences();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function prepareQuery($query, ?string $identifier = null): string
-    {
-        throw new \Exception("Null runner cannot actually run queries.");
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function doStartTransaction(int $isolationLevel = Transaction::REPEATABLE_READ):Transaction
     {
         throw new \Exception("Null runner cannot actually run queries.");
     }
